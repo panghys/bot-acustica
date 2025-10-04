cb03a1f03d45fc2c1c5e004b71852a19a2219bf6
panghys <rbgm248@gmail.com>
2025-10-04 11:03:57 -0300

    Agregar modulos como carpeta normal

---
+++ modulos/main.py
@@ -0,0 +1,91 @@
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+import subir_audios
+from discord import Member
+
+
+
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8',mode='w')
+intents = discord.Intents.default()
+intents.message_content = True # info de los mensajes
+intents.members = True # info de los usuarios
+
+bot = commands.Bot(command_prefix='!', intents = intents)
+
+
+
+@bot.event
+async def on_ready():
+    print("LISTO: BOT EN EJECUCIÓN")
+
+@bot.event
+async def on_member_join(member):
+    await member.send(f"ola {member.name}")
+@bot.event
+async def on_message(message):
+    if message.author==bot:
+        return
+    if "mensaje" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"mensaje de {message.author}!")
+
+    if "echame" in message.content.lower():
+        await message.author.kick(reason="kick")
+    await bot.process_commands(message)
+
+
+@bot.command()
+async def hola(ctx):
+    await ctx.send(f"hola {ctx.author.mention}")
+
+@bot.command()
+async def respuesta(ctx):
+    await ctx.reply("respuesta")
+
+@bot.command()
+async def poll(ctx,*,question):
+    embed = discord.Embed(title="test",description=question)
+    poll_message = await ctx.send(embed)
+    await poll_message.add_reaction("👍")
+    await poll_message.add_reaction("👎")
+
+@bot.command()
+async def audio(ctx, *, link):
+        print("recibido")
+        if ctx.author == bot.user:
+            return
+        print(f"buscando {link}")
+        archivo = await subir_audios.subirAudio(link, "audio.mp3")
+        if (archivo):
+            await ctx.send(file=discord.File(archivo))
+            os.remove(archivo)
+        else:
+            print("fail")
+            await ctx.send("error")
+
+@bot.command()
+async def status(ctx):
+    if ctx.author==bot.user:
+        return
+    await ctx.send(f"sigue andando el bot")
+    await ctx.send(f"request de {ctx.author}")
+
+
+@bot.command()
+async def echar(ctx,*,usuario: Member):
+    if ctx.author==bot.user:
+        return
+    await ctx.send(f"echando {usuario}")
+    await usuario.kick(reason="test")
+
+
+
+
+bot.run(token, log_handler=handler,log_level=logging.DEBUG)
